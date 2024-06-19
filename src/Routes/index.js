0fb84204cb0316f90ac2ab1ec29a837f9dbdea68
Maina069 <vmaina637@gmail.com>
2024-06-19 09:38:55 +0300

    first commit

---
+++ src/Routes/index.js
@@ -0,0 +1,46 @@
+import {createRouter, createWebHistory} from 'vue-router'
+
+import Homepage from "./../components/Homepage.vue"
+import products from "./../components/products.vue"
+import AboutUs  from "./../components/AboutUs.vue"
+import Contact Us from "./../components/ContactUs.vue"
+import Cart from "./../components/Cart.vue"
+import ServicesOffered  from "./../components/ServicesOffered.vue"
+
+const routes = [
+    {
+    path:'/' ,
+    name: 'Home' ,
+    component: Homepage ,
+    },
+    {
+        path:'/products' ,
+        name: 'products' ,
+        component: products ,
+    },
+    {
+        path:'/AboutUs' ,
+        name: 'AboutUs' ,
+        component: AboutUs ,
+    },
+    {
+        path:'/ContactUs' ,
+        name: 'ContactUs' ,
+        component: ContactUs ,
+    },{
+        path:'/cart' ,
+        name: 'cart' ,
+        component: cart ,
+    },
+    {
+        path:'/ServicesOffered' ,
+        name: 'ServicesOffered' ,
+        component: ServicesOffered ,
+    },
+]
+const router = createRouter({
+  history: createWebHistory(),
+  routes, 
+})
+
+export default routerimport Homepage from "./../components/Homepage.vue"
